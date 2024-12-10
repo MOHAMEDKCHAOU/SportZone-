ebda51ffd4921de429cfe22cb165123012047a45
AnIscoder01 <anisrojo519@gmail.com>
2024-12-10 19:14:03 +0100

    check changes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class gHomeController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
