e52f249222afe1bc6e33a24184ddcb21c670f52c
hamma kchaou <hamakch12@gmail.com>
2024-12-30 19:43:58 +0100

    Add Services icons in HomePage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Repository\AbonnementRepository;
 use App\Repository\SalleDeSportRepository;
-use App\Repository\SalleRepository;
+use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,15 +12,27 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(SalleDeSportRepository $salleDeSportRepository,AbonnementRepository $abonnementRepository): Response
-    {
+    public function index(
+        SalleDeSportRepository $salleDeSportRepository,
+        AbonnementRepository $abonnementRepository,
+        ServiceRepository $serviceRepository
+    ): Response {
         // Fetch all "salles" from the database
         $salles = $salleDeSportRepository->findAll();
-        $countSalle=$salleDeSportRepository->count([]);
-        $countAbonnement=$abonnementRepository->count([]);
-        // Pass "salles" to the template
+
+        // Fetch all "services" from the database
+        $services = $serviceRepository->findAll();
+
+        // Count entities
+        $countSalle = $salleDeSportRepository->count([]);
+        $countAbonnement = $abonnementRepository->count([]);
+
+        // Pass data to the template
         return $this->render('home/index.html.twig', [
-            'salles' => $salles,'countSalle'=>$countSalle,'countAbonnement'=>$countAbonnement,
+            'salles' => $salles,
+            'services' => $services,
+            'countSalle' => $countSalle,
+            'countAbonnement' => $countAbonnement,
         ]);
     }
 }
